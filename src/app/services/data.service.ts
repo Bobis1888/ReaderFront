3d4b4dd175cb8334e4f1e6d811090296eb9f042d
bobis1888 <bobis1888@gmail.com>
2021-03-08 23:52:50 +0500

    bug fix

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,14 +11,14 @@ export class DataService {
     private url: string;
 
     constructor(private http: HttpClient) {
-    	this.url = environment.baseUrl + 'api';
+    	this.url = environment.baseUrl + '/api';
     }
 
     getItems(nameSource: string): Promise<Item[]>  {
-        return this.http.get(this.url + 'items/' + nameSource).toPromise().then((response: APIResponse) => response.items);
+        return this.http.get(this.url + '/items/' + nameSource).toPromise().then((response: APIResponse) => response.items);
     }
 
     getItem(postId: number): Promise<Item> {
-        return this.http.get(this.url + 'item/' + postId).toPromise().then((response: APIResponse) => response.items[0]);
+        return this.http.get(this.url + '/item/' + postId).toPromise().then((response: APIResponse) => response.items[0]);
     }
 }
