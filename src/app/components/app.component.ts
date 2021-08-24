f1c53936a2b835280e32c2750e678a05641a1ec9
bobis1888 <bobis1888@gmail.com>
2021-08-25 00:26:39 +0500

    add sidenav timeout

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,7 +1,8 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { User } from "../models/User";
 import { AccountService } from "../services/account.service";
+import { MatSidenavContainer} from '@angular/material/sidenav';
 import { Router } from "@angular/router";
 
 @Component({
@@ -10,11 +11,12 @@ import { Router } from "@angular/router";
     styleUrls: ['./components.css'],
     providers: [AccountService]
   })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewInit {
 
   reference: any;
   mobileQuery: MediaQueryList;
   private _mobileQueryListener: () => void;
+  snavOpened: boolean = false;
 
   constructor(
       changeDetectorRef: ChangeDetectorRef,
@@ -27,7 +29,12 @@ export class AppComponent implements OnInit{
     this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.snavOpened = true;
+    },1000)
   }
 
   ngOnDestroy(): void {
